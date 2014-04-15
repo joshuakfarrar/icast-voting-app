cc77bbb2f98eb592324426a23e64c994ce65a044
sent1nel <joshua@sent1nel.me>
2014-04-15 01:15:39 -0400

    fix navbar to reflect login status

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -2,21 +2,14 @@
 
 angular.module('icastApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }, {
-      'title': 'Settings',
-      'link': '/settings'
-    }];
-    
+
     $scope.logout = function() {
       Auth.logout()
       .then(function() {
         $location.path('/login');
       });
     };
-    
+
     $scope.isActive = function(route) {
       return route === $location.path();
     };
